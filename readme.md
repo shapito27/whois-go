40c91645d5e86b57ff27fbedebf5c8bd9cb47b63
Ruslan Saifullin <r.saifullin@indoleads.com>
2022-04-29 16:19:47 +0800

    refactoring; update readme.md;

--- readme.md
+++ readme.md
@@ -1,10 +1,10 @@
-# Microservice to get Whois information
-App provides  whois information about given domain.
-It's small Golang webserver with only one endpoint:
+# Microservice providing Whois information
+This repo contains Docker image which installs Whois client on Alpine Linux distribution and Golang app.
+App is a small webserver written in Golang with only one endpoint:
 
 ```GET /whois/plain/{domain}```
 
-Endpoint uses whois package https://github.com/rfc1036/whois
+it provides whois information as a **plain text** about a given domain using whois client https://github.com/rfc1036/whois.
 
 ## Get started
 1. Download repo.
@@ -17,6 +17,10 @@ Endpoint uses whois package https://github.com/rfc1036/whois
 ```docker run -it -d -p 9091:9091 --name=whois-go whois-go```
 
 ### How to use
-Send request to
+Send request to endpoint like this:
 
 ```curl 0.0.0.0:9091/whois/plain/facebook.com```
+
+port - optional GET param to let know Whois client to use it instead of default 43 port
+
+```curl 0.0.0.0:9091/whois/plain/google.ch?port=4343```
--- server.go
+++ server.go
@@ -2,46 +2,57 @@ package main
 
 import (
 	"fmt"
-	"github.com/gorilla/mux"
 	"net/http"
 	"os/exec"
+
+	"github.com/gorilla/mux"
 )
 
+const (
+	addr = ":9091"
+	whoisRoute = "/whois/plain/{domain}"
+)
+
+// main runs server on fiven ip and port
+func main() {
+	// setup routing
+	router := mux.NewRouter()
+	router.HandleFunc(whoisRoute, getWhoisPlain)
+	http.Handle("/", router)
+
+	err := http.ListenAndServe(addr, nil) 
+	if err != nil {
+		fmt.Println("\n\n", err)
+	}
+}
+
+// getWhoisPlain outputs whois information by given domain name and port.
+// Example: /whois/plain/google.com?port=9999
 func getWhoisPlain(w http.ResponseWriter, r *http.Request) {
-    if r.Method == "POST" {
-		fmt.Fprintf(w, "POST method is not allowed")
+	if r.Method != "GET" {
+		fmt.Fprintf(w, "Only GET requests are allowed")
+		return
 	}
 
 	vars := mux.Vars(r)
 	var err error
 	var out []byte
 
-	port:= r.URL.Query().Get("port")
+	port := r.URL.Query().Get("port")
 
 	if port != "" {
-	    out, err = exec.Command("whois", "-H", vars["domain"], "-p", port).Output()
+		out, err = exec.Command("whois", "-H", vars["domain"], "-p", port).Output()
 	} else {
-	    out, err = exec.Command("whois", "-H", vars["domain"]).Output()
+		out, err = exec.Command("whois", "-H", vars["domain"]).Output()
 	}
 
-    checkError(err, "Whois failed. Err:")
+	checkError(err, "Whois failed. Err:")
 
-    // send data to client side
+	// send data to client side
 	fmt.Fprintf(w, "%s", string(out))
 }
 
-func main() {
-	//routing
-	router := mux.NewRouter()
-	router.HandleFunc("/whois/plain/{domain}", getWhoisPlain)
-	http.Handle("/", router)
-
-	err := http.ListenAndServe(":9091", nil) // set listen ip and port
-	if err != nil {
-		fmt.Println("\n\n", err)
-	}
-}
-
+// checkError check if error and output it
 func checkError(err error, mes string) {
 	if err != nil {
 		fmt.Println(mes, err)
